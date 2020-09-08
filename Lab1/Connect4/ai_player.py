7b6d077b8e14f0fc08d08a143ff31ecebb481a3e
Armel Chausse <armel.chausse@student.ecp.fr>
2020-09-08 12:14:22 +0200

    Adding new AI for fun

--- Lab1/Connect4/ai_player.py
+++ Lab1/Connect4/ai_player.py
@@ -1,35 +1,23 @@
 from player import Player
 from copy import deepcopy
-import time
-import traceback
-import random
-from board import Board
 
 
 class AIPlayer(Player):
-    """This player should implement a heuristic along with a min-max and alpha
-    beta search to"""
-
     def __init__(self):
-        self.name = "Venus"
-        self.max_depth = 2
-        self.valeurs = [0, 1, 5, 40, 1000, 1e5]
+        self.name = "Lemraus"
+        self.max_depth = 3
+        self.streak_weights = [0, 1, 15, 100, 2000]
 
     def getColumn(self, board):
-        timestamp_start = time.time()
-
         alpha = -1e6
         best_col = None
 
         for col in board.getPossibleColumns():
             next_board = deepcopy(board)
-            next_board.play(self.color, col)
-            if self.score(deepcopy(next_board)) > 500:
+            row = next_board.play(self.color, col)
+            if self.get_play_score(row, col, next_board) > 1000:
                 return col
-            try:
-                beta = self.max_step(next_board, 0, 1e6, alpha)
-            except:
-                traceback.print_exc()
+            beta = self.min_step(next_board, 1, 1e6, alpha)
             if beta > alpha:
                 alpha = beta
                 best_col = col
@@ -40,46 +28,45 @@ class AIPlayer(Player):
         alpha = -1e6
         for col in board.getPossibleColumns():
             next_board = deepcopy(board)
-            next_board.play(-self.color, col)
-            next_board_score = self.score(deepcopy(next_board))
-            if next_board_score > 500:
-                return next_board_score
+            row = next_board.play(self.color, col)
+            score = self.get_play_score(row, col, next_board)
+            if score > 1000:
+                return score
             if depth == self.max_depth:
-                alpha_candidate = next_board_score
+                beta = score
             else:
-                alpha_candidate = self.min_step(
+                beta = self.min_step(
                     next_board, depth + 1, min_beta, max(alpha, max_alpha)
                 )
-            if alpha_candidate > min_beta:
-                return alpha_candidate
-            if alpha_candidate > alpha:
-                alpha = alpha_candidate
+            if beta > min_beta:
+                return beta
+            if beta > alpha:
+                alpha = beta
         return alpha
 
     def min_step(self, board, depth, min_beta, max_alpha):
         beta = 1e6
         for col in board.getPossibleColumns():
             next_board = deepcopy(board)
-            next_board.play(self.color, col)
-            next_board_score = self.score(deepcopy(next_board))
-            if next_board_score < -500:
-                return next_board_score
+            row = next_board.play(-self.color, col)
+            score = self.get_play_score(row, col, next_board)
+            if score < -1000:
+                return score
             if depth == self.max_depth:
-                beta_candidate = next_board_score
+                alpha = score
             else:
-                beta_candidate = self.max_step(
+                alpha = self.max_step(
                     next_board, depth + 1, min(beta, min_beta), max_alpha
                 )
 
-            if beta_candidate < max_alpha:
-                return beta_candidate
-            if beta_candidate < beta:
-                beta = beta_candidate
+            if alpha < max_alpha:
+                return alpha
+            if alpha < beta:
+                beta = alpha
         return beta
 
-    def score(self, board):
-        # base
-        score_board = [
+    def get_play_score(self, row, col, board):
+        weights = [
             [5, 8, 11, 13, 11, 8, 5],
             [5, 8, 11, 13, 11, 8, 5],
             [4, 6, 8, 10, 8, 6, 4],
@@ -87,94 +74,46 @@ class AIPlayer(Player):
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
         ]
-        S = 0
-        # print(
-        #     "Temps pris pour effectuer les calculs : ", time.time() - timestamp_start,
-        # )
+        row_score = self.get_row_score(row, col, board)
+        col_score = self.get_col_score(row, col, board)
+        diag_score = self.get_diag_score(row, col, board)
+        score = weights[row][col] + row_score + col_score + diag_score
+        return score
 
-        for i in range(6):
-            for j in range(7):
-                S += score_board[i][j] * board.getRow(i)[j]
-        # alignements
-        S += self.xplore_Diag_up(board)
-        S += self.xplore_columns_smart(board)
-        S += self.xplore_lines_smart(board)
+    def get_streaks_score(self, l):
+        streak = 0
+        score = 0
+        prev_val = 0
+        for val in l:
+            if val == 0:
+                prev_val = 0
+                streak = 0
+                continue
+            if val != prev_val:
+                streak = 0
+            streak += 1
+            score += self.color * val * self.streak_weights[min(4, streak)]
+            prev_val = val
+        return score
 
-        return S
+    def get_row_score(self, row, col, board):
+        row_slice = board.getRow(row)[max(0, col - 3) : min(6, col + 4)]
+        return self.get_streaks_score(row_slice)
 
-    def xplore_columns_smart(self, board):
-        bonus_columns = 0
+    def get_col_score(self, row, col, board):
+        col_slice = board.getCol(col)[max(0, row - 3) : min(5, row + 4)]
+        return self.get_streaks_score(col_slice)
 
-        for j in board.getPossibleColumns():
+    def get_diag_score(self, row, col, board):
+        diag_up_score = self.get_diag_up_score(row, col, board)
+        diag_down_score = self.get_diag_down_score(row, col, board)
+        return diag_up_score + diag_down_score
 
-            # getting column without zeros
-            col = deepcopy(board.getCol(j))
-            while col and col[-1] == 0:
-                col.pop()
+    def get_diag_up_score(self, row, col, board):
+        diag_up = board.getDiagonal(True, col - row)
+        return self.get_streaks_score(diag_up)
 
-            if col:  # Continue si la colonne est non vide
-                top_pin = col.pop()
-                streak = 1
+    def get_diag_down_score(self, row, col, board):
+        diag_down = board.getDiagonal(False, col + row)
+        return self.get_streaks_score(diag_down)
 
-                while col and top_pin == col.pop():
-                    streak += 1
-                # on ajoute la valeur
-                bonus_columns += top_pin * self.valeurs[streak]
-
-        return bonus_columns
-
-    def xplore_Diag_up(self, board):
-        diag_bonus = 0
-        for i in range(-5, 7):
-            liste = board.getDiagonal(True, i)
-
-            allie_streak = 0
-            adv_streak = 0
-            for j in liste:  # incrémentation des streaks aliés et adv
-                if j == self.color:
-                    allie_streak += 1
-                    adv_streak = 0
-                if j == -self.color:
-                    adv_streak += 1
-                    allie_streak = 0
-            diag_bonus += (allie_streak * self.valeurs[allie_streak]) - (
-                adv_streak * self.valeurs[adv_streak]
-            )
-            allie_streak = 0
-            adv_streak = 0
-        diag_down_bonus = self.xplore_Diag_down(board, sum_bonus=diag_bonus)
-        return diag_bonus + diag_down_bonus
-
-    def xplore_Diag_down(self, board, sum_bonus):
-        diag_bonus = 0
-        for i in range(0, 12):
-            liste = board.getDiagonal(False, i)
-            allie_streak = 0
-            adv_streak = 0
-            for j in liste:  # incrémentation des streaks aliés et adv
-                if j == self.color:
-                    allie_streak += 1
-                    adv_streak = 0
-                if j == -self.color:
-                    adv_streak += 1
-                    allie_streak = 0
-
-            diag_bonus += (allie_streak * self.valeurs[allie_streak]) - (
-                adv_streak * self.valeurs[adv_streak]
-            )
-        return diag_bonus
-
-    def xplore_lines_smart(self, board):
-        bonus_lines = 0
-        for i in range(6):
-            line = list(filter(lambda x: x != 0, board.getRow(i)))
-            if line:
-                for streak in range(2, 5):  # print(streak)
-                    for j in range(7 - streak):
-                        k = sum(line[j : j + streak])
-                        # print("   ", k)
-                        if k >= streak:
-                            bonus_lines += self.valeurs[streak]
-                        elif k <= -(streak):
-                            bonus_lines -= self.valeurs[streak]
-        return bonus_lines
--- Lab1/Connect4/run_cmd.py
+++ Lab1/Connect4/run_cmd.py
@@ -3,20 +3,21 @@ import argparse
 from game import Game
 from player import HumanPlayer, RandomPlayer
 from ai_player import AIPlayer
+from venus import AIPlayer as VenusAI
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--p1', default='player 1')
-    parser.add_argument('--p2', default='player 2')
-    parser.add_argument('--rows', default='6', type=int)
-    parser.add_argument('--cols', default='7', type=int)
-    parser.add_argument('--num', default='4', type=int)
+    parser.add_argument("--p1", default="player 1")
+    parser.add_argument("--p2", default="player 2")
+    parser.add_argument("--rows", default="6", type=int)
+    parser.add_argument("--cols", default="7", type=int)
+    parser.add_argument("--num", default="4", type=int)
     args = parser.parse_args()
 
     player1 = AIPlayer()
     player1.name = args.p1
-    player2 = RandomPlayer()
+    player2 = VenusAI()
     player2.name = args.p2
     game = Game(player1, player2, args.cols, args.rows, args.num, verbose=True)
     game.run()
